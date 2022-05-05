afe0cdba290eb46f4bbbcc020acf5673499812b9
Anastasia_Kolevatykh <anastasia_kolevatykh@epam.com>
2022-05-05 09:38:31 +0300

    Adjust project structure

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ The application is used for the following actions:
 
 ## Implementation details:
 
-Implementation is designed for 2 various HTTP clients: Jersey 2 (Jax-RS) and OpenFeign.
+Implementation is designed for 2 various HTTP clients: Jersey (Jax-RS) and OpenFeign.
 
 ### Build
 
@@ -23,3 +23,33 @@ Set properties in file named `gradle.properties` that can be found by path `src/
 ### Run
 
     ./gradlew run
+
+### Jersey vs. OpenFeign
+
+_Jersey_
+
+    Pros:
+        - "JSR standard can be run without servlet container"
+        - "Production-ready implementation designed for REST applications only"
+        - Jersey2 is easily converted to Jersey3, just need to replace the javax.ws.rs to jakarta
+        - Interceptors and Filters are easy to use 
+
+    Cons:
+        - Extra code for simple tasks
+        - The need to explicitly process each query param for each specific request
+        - Entity is hard to conver to byte array 
+        - Need to use Interceptor or Filter in order to handle extra work with request/response
+
+---
+_OpenFeign_
+
+    Pros:
+        - Easy to implement
+        - "A higher level of abstraction over REST-based service calls"
+        - "A declarative way to build HTTP clients"
+        - "URLs are not hardcoded"
+        - Query params are flexible to use by including into path variable
+        - Source code is written in Kotlin and therefore smoothly compatable with Gradle
+
+    Cons:
+        - "it does not support caching based on Cache Control headers" stackoverflow
--- src/main/java/com/sumsub/test/SumSubApplicantMain.java
+++ src/main/java/com/sumsub/test/SumSubApplicantMain.java
@@ -26,58 +26,59 @@ public class SumSubApplicantMain {
     public static final Logger log = LoggerFactory.getLogger(SumSubApplicantMain.class);
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
-    private static final ApplicantFacade<JerseyClientService> jerseyApplicant = new ApplicantService();
-    private static final ApplicantFacade<FeignClientService> feignApplicant = new FeignApplicantService();
+    private static final ApplicantFacade<JerseyClientService> jerseyApplicantService = new ApplicantService();
+    private static final ApplicantFacade<FeignClientService> feignApplicantService = new FeignApplicantService();
 
-    private static final DocumentFacade<JerseyClientService> jerseyDocument = new DocumentService();
-    private static final DocumentFacade<FeignClientService> feignDocument = new FeignDocumentService();
+    private static final DocumentFacade<JerseyClientService> jerseyDocumentService = new DocumentService();
+    private static final DocumentFacade<FeignClientService> feignDocumentService = new FeignDocumentService();
 
-    private static final TokenFacade<JerseyClientService> jerseyToken = new TokenService();
-    private static final TokenFacade<FeignClientService> feignToken = new FeignTokenService();
+    private static final TokenFacade<JerseyClientService> jerseyTokenService = new TokenService();
+    private static final TokenFacade<FeignClientService> feignTokenService = new FeignTokenService();
 
     public static void main(String[] args) throws Exception {
-        Applicant applicantJ = Applicant.builder().build();
-        Applicant applicantF = Applicant.builder().build();
-        File file = new File(Objects.requireNonNull(SumSubApplicantMain.class.getResource("/docs/passport.jpg")).getFile());
+        Applicant jerseyApplicant = Applicant.builder().build();
+        Applicant feignApplicant = Applicant.builder().build();
+        File jerseyFile = new File(Objects.requireNonNull(SumSubApplicantMain.class.getResource("/docs/passport.jpg")).getFile());
+        File feignFile = new File(Objects.requireNonNull(SumSubApplicantMain.class.getResource("/docs/passport2.jpg")).getFile());
 
         // Jersey
-        Applicant createdJerseyApplicant = jerseyApplicant.createApplicant(applicantJ);
-        log.info("[Jersey] Created an applicant: " + createdJerseyApplicant.id + " for externalUserId: " + applicantJ.externalUserId);
+        Applicant createdJerseyApplicant = jerseyApplicantService.createApplicant(jerseyApplicant);
+        log.info("[Jersey] Created an applicant: " + createdJerseyApplicant.id + " for externalUserId: " + jerseyApplicant.externalUserId);
         // Feign
-        Applicant createdFeignApplicant = feignApplicant.createApplicant(applicantF);
-        log.info("[Feign] Created an applicant: " + createdFeignApplicant.id + " for externalUserId: " + applicantF.externalUserId);
+        Applicant createdFeignApplicant = feignApplicantService.createApplicant(feignApplicant);
+        log.info("[Feign] Created an applicant: " + createdFeignApplicant.id + " for externalUserId: " + feignApplicant.externalUserId);
 
         // Jersey
-        String addedJerseyDocument = jerseyDocument.addDocument(createdJerseyApplicant.id, file);
+        String addedJerseyDocument = jerseyDocumentService.addDocument(createdJerseyApplicant.id, jerseyFile);
         log.info("[Jersey] Identifier of the added document: " + addedJerseyDocument);
         // Feign
-        String addedFeignDocument = feignDocument.addDocument(createdFeignApplicant.id, file);
+        String addedFeignDocument = feignDocumentService.addDocument(createdFeignApplicant.id, feignFile);
         log.info("[Feign] Identifier of the added document: " + addedFeignDocument);
 
         // Jersey
-        String jerseyApplicantStatus = jerseyApplicant.getApplicantStatus(createdJerseyApplicant.id);
+        String jerseyApplicantStatus = jerseyApplicantService.getApplicantStatus(createdJerseyApplicant.id);
         log.info("[Jersey] Applicant status: " + jerseyApplicantStatus);
         // Feign
-        String feignApplicantStatus = feignApplicant.getApplicantStatus(createdFeignApplicant.id);
+        String feignApplicantStatus = feignApplicantService.getApplicantStatus(createdFeignApplicant.id);
         log.info("[Feign] Applicant status (json string): " + feignApplicantStatus);
 
         // Jersey
-        String jerseyAccessToken = jerseyToken.getAccessToken(createdJerseyApplicant.externalUserId);
+        String jerseyAccessToken = jerseyTokenService.getAccessToken(createdJerseyApplicant.externalUserId);
         log.info("[Jersey] Access token (json string): " + jerseyAccessToken);
         // Feign
-        String getFeignAccessToken = feignToken.getAccessToken(createdFeignApplicant.externalUserId);
+        String getFeignAccessToken = feignTokenService.getAccessToken(createdFeignApplicant.externalUserId);
         log.info("[Feign] Access token (json string): " + getFeignAccessToken);
 
         // Jersey
-        String jerseyApplicantStatusPending = jerseyApplicant.setApplicantStatusPending(createdJerseyApplicant.id);
+        String jerseyApplicantStatusPending = jerseyApplicantService.setApplicantStatusPending(createdJerseyApplicant.id);
         log.info("[Jersey] Applicant status to pending: " + jerseyApplicantStatusPending);
         // Feign
-        String feignApplicantStatusPending = feignApplicant.setApplicantStatusPending(createdFeignApplicant.id);
+        String feignApplicantStatusPending = feignApplicantService.setApplicantStatusPending(createdFeignApplicant.id);
         log.info("[Feign] Applicant status to pending: " + feignApplicantStatusPending);
 
-        Thread.sleep(Long.parseLong(PropertyUtil.getProperty("thread_sleep")));
+        Thread.sleep(Long.parseLong(PropertyUtil.getProperty("timeout")));
         // Jersey
-        Object jerseyApplicantInfo = jerseyApplicant.getApplicantInfo(createdJerseyApplicant.id);
+        Object jerseyApplicantInfo = jerseyApplicantService.getApplicantInfo(createdJerseyApplicant.id);
         log.info("[Jersey] Get an applicant info: " + jerseyApplicantInfo + " for data: " + createdJerseyApplicant.externalUserId);
         Map<String, Object> jerseyInfo = objectMapper.convertValue(jerseyApplicantInfo, LinkedHashMap.class);
         Map<String, String> jerseyDocInfo = objectMapper.convertValue(jerseyInfo.get("info"), LinkedHashMap.class);
@@ -85,7 +86,7 @@ public class SumSubApplicantMain {
                 + ", last name = " + jerseyDocInfo.get("lastNameEn"));
 
         // Feign
-        Object feignApplicantInfo = feignApplicant.getApplicantInfo(createdFeignApplicant.id);
+        Object feignApplicantInfo = feignApplicantService.getApplicantInfo(createdFeignApplicant.id);
         log.info("[Feign] Get an applicant info: " + feignApplicantInfo + " for data: " + createdFeignApplicant.externalUserId);
         Map<String, Object> feignInfo = objectMapper.convertValue(feignApplicantInfo, LinkedHashMap.class);
         Map<String, String> feignDocInfo = objectMapper.convertValue(feignInfo.get("info"), LinkedHashMap.class);
